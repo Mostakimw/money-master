96da156f35122ad1ce9b35a479191f2ed6a81a2f
Mostakimw <scientist.expert@gmail.com>
2023-02-16 13:21:53 +0600

    all done

--- js/app.js
+++ js/app.js
@@ -24,6 +24,7 @@ document.getElementById('calculate-btn').addEventListener('click', function(){
     const rent = getInputValue('rent-input')
     const clothes = getInputValue('clothes-input')
     var expenses = food + rent + clothes;
+
     const income = getInputValue('income-input')
     var balance = income - expenses;
     if(isNaN(income) || income < 0 || isNaN(food) || isNaN(rent) || isNaN(clothes) || food < 0 || rent < 0 || clothes < 0){
@@ -31,7 +32,7 @@ document.getElementById('calculate-btn').addEventListener('click', function(){
     }
     // ! total expense 
     else{
-        if(expenses > balance){
+        if(expenses > income){
             alert('Your expenses more than your income. You cannot expense more than your income. So please reduce your expenses.')
         }
         else {
@@ -45,16 +46,39 @@ document.getElementById('calculate-btn').addEventListener('click', function(){
 // ! save btn handler
 document.getElementById('save-btn').addEventListener('click', function(){
     const save = getInputValue('save-input');
-    const income = getInputValue('income-input')
-    const saving = (save / 100) * income;
+    const balanceElement = element('balance');
+    const balance = parseInt(balanceElement.innerText);
     if(isNaN(save) || save < 0){
         alert('Please fill all input with proper value. You have to fill all input with Number not string')
     }
     else{
-        setInnerText('saving-amount', saving);
-        const balanceString = element('balance');
-        const balance = parseInt(balanceString.innerText);
-        const remainingBalance = balance - saving;
-        setInnerText('remaining-balance', remainingBalance)
+        if(save > 100){
+            alert('You cannot save greater than 100%')
+        }
+        else{
+            if(calculateSaving() > balance){
+                alert('You cannot save more than your remaining balance after spend')
+            }
+            else {
+                setSavingAndRemainingBalance();
+            }
+        }
     }
 })
+
+// ! calculate saving 
+function calculateSaving(){
+    const save = getInputValue('save-input');
+    const income = getInputValue('income-input')
+    const saving = (save / 100) * income;
+    return saving;
+}
+
+// ! set remaining balance 
+function setSavingAndRemainingBalance(){
+        setInnerText('saving-amount', calculateSaving());
+        const balanceString = element('balance');
+        const balance = parseInt(balanceString.innerText);
+        const remainingBalance = balance - calculateSaving();
+        setInnerText('remaining-balance', remainingBalance)
+}
